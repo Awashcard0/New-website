6c03f21ffd0f8dd402987dcc3457cfc2a4f5ac02
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-05-15 14:08:27 -0400

    I needed a S

--- projects/digital-signage/app/script.js
+++ projects/digital-signage/app/script.js
@@ -1,4 +1,4 @@
-let data = {id: "hi", type: "iframe", data: "http://awashcard0.ga"}
+let data = {id: "hi", type: "iframe", data: "https://awashcard0.ga"}
 
         setTimeout(function() {
             document.getElementById("start").showModal();
