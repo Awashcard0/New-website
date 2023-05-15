8a3856e6e510cecc4949da9adca08b54dcd6c765
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-05-15 10:56:15 -0400

    ok

--- projects/digital-signage/app/script.js
+++ projects/digital-signage/app/script.js
@@ -23,73 +23,18 @@ noload();
                 document.write(`${data.data}`);
                 noload()
             } else if (data.type == "text") {
-                document.write(`<h1>${data.data}</h1>
-                <script>
-                    /*!	
-* FitText.js 1.0 jQuery free version
-*
-* Copyright 2011, Dave Rupert http://daverupert.com 
-* Released under the WTFPL license 
-* http://sam.zoy.org/wtfpl/
-* Modified by Slawomir Kolodziej http://slawekk.info
-*
-* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
-*/
-(function(){
+                document.write(`
+                <style>
+                svg {
+                    width: 100%;
+                  }
 
-  var addEvent = function (el, type, fn) {
-    if (el.addEventListener)
-      el.addEventListener(type, fn, false);
-		else
-			el.attachEvent('on'+type, fn);
-  };
-  
-  var extend = function(obj,ext){
-    for(var key in ext)
-      if(ext.hasOwnProperty(key))
-        obj[key] = ext[key];
-    return obj;
-  };
-
-  window.fitText = function (el, kompressor, options) {
-
-    var settings = extend({
-      'minFontSize' : -1/0,
-      'maxFontSize' : 1/0
-    },options);
-
-    var fit = function (el) {
-      var compressor = kompressor || 1;
-
-      var resizer = function () {
-        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Bind events
-      // If you have any js library which support Events, replace this part
-      // and remove addEvent function (or use original jQuery version)
-      addEvent(window, 'resize', resizer);
-      addEvent(window, 'orientationchange', resizer);
-    };
-
-    if (el.length)
-      for(var i=0; i<el.length; i++)
-        fit(el[i]);
-    else
-      fit(el);
-
-    // return set of elements
-    return el;
-  };
-})();
-
-setTimeout(function() {
-    fitText(document.querySelector("h1"), 0.38);       
-}, 1000);
-                    </script>`);
+                  <svg viewBox="0 0 56 18">
+  <text x="0" y="15">${data.data}</text>
+</svg>
+                </style>
+                
+                `);
                 noload();
             } else if (data.type == "video") {
                 document.write(`<video autoplay loop id="myVideo">
