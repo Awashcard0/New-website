35c281630435085c775a741c57771df2ccf7ff51
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-05-22 09:47:21 -0400

    hi

--- projects/digital-signage/app/script.js
+++ projects/digital-signage/app/script.js
@@ -17,7 +17,7 @@ let id = window.location.href.split("?id=")[1]
 
         function get(oc) {
             data = oc;
-            alert(data[id]);
+            alert(data.hi);
                 setTimeout(function() {
                     load();       
             }, 1000);
