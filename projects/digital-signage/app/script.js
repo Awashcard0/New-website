ab3f4d2914faf8cee21dbf24b5e1f47de517de0f
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-06-07 10:49:48 -0400

    better

--- projects/digital-signage/app/script.js
+++ projects/digital-signage/app/script.js
@@ -82,12 +82,10 @@ body {
 </style>`);
 noload();
             } else {
-                document.getElementById("Ename").innerHTML = "Error getting data<br>Reloading...";
-                document.getElementById("load").showModal();
-                setTimeout(function() {
-                    // for some resone location.reload() dose not work in a .js file
-                    reload();
-            }, 1000);
+                document.write(`<iframe src="https://awashcard0.pages.dev/projects/digital-signage/app/error.html" allow="accelerometer; autoplay; encrypted-media; gyroscope;" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+                Your browser doesn't support iframes
+            </iframe>`);
+            noload();
             }
         }
 
