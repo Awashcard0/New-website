8f3aea979d8052e5bf215208294d760fec85c007
Awashcard0 <lumpy27@outlook.com>
2023-07-05 23:01:00 -0400

    Home thing

--- home-thing/script.js
+++ home-thing/script.js
@@ -1,3 +1,5 @@
+let homeThingOpen = false;
+
     document.addEventListener("DOMContentLoaded", function(e) {
     var homeButton = document.createElement("btn");
     homeButton.setAttribute('tabindex', 0);
@@ -6,31 +8,46 @@
     homeButton.setAttribute('id', 'noKill');
     // homeButton.setAttribute('title', 'Hover to see user count for every awashcard0 site');
     homeButton.addEventListener('click', function(e) {
-      document.body.classList.add('blockscreen');
-      homeButton.addEventListener('transitionend', function(e) {
-        window.requestAnimationFrame(function() {
-          // Try forcing repaint
-          homeButton.getBoundingClientRect();
-          let utm = location.href
-          document.getElementById("noKill").style.removeProperty('background-color');
-          document.getElementById("noKill").style.removeProperty('backgroundColor');
-          if (location.hostname == "awashcard0.ga" || location.hostname == "awashcard0.pages.dev") {
-            var myDivs = document.getElementsByClassName("blockscreen");
-                for (var i = 0; i < myDivs.length; i++) {
-                myDivs[i].style.backgroundColor = "#00ff77";
-                }
-                removeAll();
-            window.location = `/?utm=${utm}`;
-          } else {
-            var myDivs = document.getElementsByClassName("blockscreen");
-                for (var i = 0; i < myDivs.length; i++) {
-                myDivs[i].style.backgroundColor = "#3217ff";
-                }
-                removeAll();
-            window.location = `http://awashcard0.pages.dev/?utm=${utm}`;
-          }
-        });
-      });
+
+      if (!homeThingOpen) {
+        var settingsPage = document.createElement("div");
+        settingsPage.setAttribute('tabindex', 0);
+        settingsPage.setAttribute('class', "settings-background");
+        document.body.appendChild(settingsPage);
+  
+        settingsPage.innerHTML = `
+        <h1>Settings</h1>
+        <span class="close" onclick="closeHomeThing(this.parentElement)">×</span>
+        <a href="https://awashcard0.pages.dev/" class="homeLink">Go home</a>
+        `;
+      }
+
+      homeThingOpen = true;
+      // document.body.classList.add('blockscreen');
+      // homeButton.addEventListener('transitionend', function(e) {
+      //   window.requestAnimationFrame(function() {
+      //     // Try forcing repaint
+      //     homeButton.getBoundingClientRect();
+      //     let utm = location.href
+      //     document.getElementById("noKill").style.removeProperty('background-color');
+      //     document.getElementById("noKill").style.removeProperty('backgroundColor');
+      //     if (location.hostname == "awashcard0.ga" || location.hostname == "awashcard0.pages.dev") {
+      //       var myDivs = document.getElementsByClassName("blockscreen");
+      //           for (var i = 0; i < myDivs.length; i++) {
+      //           myDivs[i].style.backgroundColor = "#00ff77";
+      //           }
+      //           removeAll();
+      //       window.location = `/?utm=${utm}`;
+      //     } else {
+      //       var myDivs = document.getElementsByClassName("blockscreen");
+      //           for (var i = 0; i < myDivs.length; i++) {
+      //           myDivs[i].style.backgroundColor = "#3217ff";
+      //           }
+      //           removeAll();
+      //       window.location = `http://awashcard0.pages.dev/?utm=${utm}`;
+      //     }
+      //   });
+      // });
     });
     homeButton.addEventListener('keydown', function(e) {
       if (e.keyCode === 13) homeButton.click();
@@ -63,18 +80,19 @@ homeButton.addEventListener("mouseleave", function(e) {
     }
   }
 
-  // function updateusers() {
-  //   if (showUsers) {
-  //     var usersCount = document.createElement("span");
-  //     usersCount.setAttribute('tabindex', 0);
-  //     usersCount.setAttribute('aria-label', 'Go to main directory');
-  //     usersCount.setAttribute('id', 'userThing');
-  //     document.body.appendChild(usersCount);
-  //     document.getElementById("userThing").innerHTML = "Users online: " + usersOnline;
-  //   } else {
-  //     document.getElementById("userThing").remove();
-  //   }
-  // }
+  function updateusers() {
+    if (showUsers) {
+      var usersCount = document.createElement("span");
+      usersCount.setAttribute('tabindex', 0);
+      usersCount.setAttribute('aria-label', 'Go to main directory');
+      usersCount.setAttribute('id', 'userThing');
+      document.body.appendChild(usersCount);
+      // document.getElementById("userThing").innerHTML = "Users online: " + usersOnline;
+      document.getElementById("userThing").innerHTML = "Not working";
+    } else {
+      document.getElementById("userThing").remove();
+    }
+  }
 
   var usersOnline = 1;
 
@@ -91,7 +109,7 @@ homeButton.addEventListener("mouseleave", function(e) {
   //     if (showUsers) {document.getElementById("userThing").innerHTML = "Users online: " + usersOnline};
   //   }, 5000);
 
-fetch('/info.json')
+fetch('https://awashcard0.pages.dev/info.json')
   .then(response => response.json())
   .then(info => gotInfo(info))
   .catch(error => console.error(error));
@@ -119,9 +137,14 @@ fetch('/info.json')
       var calloutClose = document.createElement("span");
       calloutClose.setAttribute('tabindex', 0);
       calloutClose.setAttribute('class', 'callout-closebtn');
-      calloutClose.setAttribute('onclick', 'this.parentElement.style.display="none"')
+      calloutClose.setAttribute('onclick', 'body.removeChild(this.parentElement)')
       calloutClose.innerText = "×";
       callout.appendChild(calloutClose);
       
     }
+  }
+
+  function closeHomeThing(element) {
+    homeThingOpen = false;
+    document.body.removeChild(element);
   }
