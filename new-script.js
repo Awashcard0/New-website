48a9042887737bc6fa2574d502a14c4a6885c345
Thunderlord677 <Thunderwing289games@gmail.com>
2024-05-04 15:38:57 -0400

    added shift+enter autocomplete for commands and added a description to commands

--- new-script.js
+++ new-script.js
@@ -1,10 +1,20 @@
 // I Just You For My Own, More Than You Could Ever Know, Make My Wish Come True, All I Want For Christmas Is You!!
 // for proper javascript knowledge you should be listening to this playlist: https://open.spotify.com/playlist/2UvaLj0iFyLEXhKJQysYTc?si=d03e705244804068
 const commands = ["help", "echo", "cls", "clear", "history", "neofetch", "cat", "ls"];
+const info = {
+    "help": "This command will list all the commands.",
+    "echo": "This command will output whatever you type after it.",
+    "cls": "This command will clear the screen.",
+    "neofetch": "does stuff",
+    "history": "fetches your command history.",
+    "cat": "read files from the file system.",
+    "ls": "list files from the file system."
+}
 let history = [];
 const fs = new Map();
 
 fs.set("aboutMe.txt", "Hello! I'm Awashard0, a self-taught developer. <br> I like gaming with my friends and coding. ");
+fs.set("controls.txt", "shift+enter autocomplete commands <br>TODO: autocomplete arguments</br>");
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get the varibles
@@ -16,14 +26,26 @@ document.addEventListener("DOMContentLoaded", function() {
         event.preventDefault();
         inputBox.focus();
     });
+    
 
     inputBox.addEventListener("keypress", function(event) {
-            // If the user presses the "Enter" key on the keyboard
-            if (event.key === "Enter") {
-                event.preventDefault();
-                // If the input is not empty
-                if (inputBox.value) {
-                    let val = inputBox.value;
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+            event.preventDefault();
+            // If the input is not empty
+            if (inputBox.value) {
+                // Get the value of the textbox
+                let val = inputBox.value;
+                // Receive holding shift
+                if (event.shiftKey) {
+                    // find the closest command to the current input value
+                    let closest = commands.find(cmd => cmd.startsWith(val)); // TODO: if multiple commands match, cycle through
+                    // if found
+                    if (closest) {
+                        // replace the curent input value with the closest command
+                        inputBox.value = closest;
+                    } 
+                } else {
                     // Add the input value to the list item
                     output(`<span style="color: rgb(27, 219, 153);">awash@awashpc</span>:<span style="color: rgb(61, 173, 233);">~</span>$ ` + val);
                     // Clear the input
@@ -34,11 +56,11 @@ document.addEventListener("DOMContentLoaded", function() {
                     checkCommand(val);
                     // Add the command to the history
                     history.push(val);
-
                 }
             }
         }
-    );
+    }
+);
 });
 
 function checkCommand(input) {
@@ -55,7 +77,11 @@ function checkCommand(input) {
             output(out);
             break;
         case "help":
-            output("Available Commands: " + commands.join(", "));
+            let helpText = "Available Commands: ";
+            for (let command in info) {
+                helpText += `<br>${command}: ${info[command]}</br>`;
+            }
+            output(helpText);
             break;
         case "cls":
         case "clear":
