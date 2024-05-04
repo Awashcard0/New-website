6ea0024fb23c78343dcb3a20cef15cb4c1aac09b
Awashcard0 <lumpy27@outlook.com>
2024-05-03 22:08:07 -0400

    Color

--- new-script.js
+++ new-script.js
@@ -1,7 +1,10 @@
 // I Just You For My Own, More Than You Could Ever Know, Make My Wish Come True, All I Want For Christmas Is You!!
 // for proper javascript knowledge you should be listening to this playlist: https://open.spotify.com/playlist/2UvaLj0iFyLEXhKJQysYTc?si=d03e705244804068
-const commands = ["help, echo"]
+const commands = ["help", "echo", "cls", "clear", "history"]
 let history = [];
+const fs = new Map();
+
+fs.set("aboutMe.txt", "Hello! I'm Awashard0, a self-taught developer. <br> I like gaming with my friends and coding. ");
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get the varibles
@@ -22,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 if (inputBox.value) {
                     let val = inputBox.value;
                     // Add the input value to the list item
-                    output("awash@awashpc:$ " + val);
+                    output(`<span style="color: rgb(27, 219, 153);">awash@awashpc</span>:<span style="color: rgb(61, 173, 233);">~</span>$ ` + val);
                     // Clear the input
                     inputBox.value = "";
                     // Hide the input to let the command run
@@ -46,6 +49,7 @@ function checkCommand(input) {
     let cmd = Fullcmd.split(" ")[0];
 
     switch (cmd) {
+        // General Commands
         case "echo":
             const out = input.split(" ").slice(1).join(" ");
             output(out);
@@ -54,11 +58,26 @@ function checkCommand(input) {
             output("Available Commands: " + commands.join(", "));
             break;
         case "cls":
+        case "clear":
             list.innerHTML = "";
             break;
+        case "history":
+            output(history.map((cmdinh, index) => `${index + 1}. ${cmdinh}`).join("\n"));
+            break;
+        // File System Commands
+        case "cat":
+            const file = input.split(" ").slice(1).join(" ");
+            if (fs.has(file)) {
+                output(fs.get(file));
+            } else {
+                output("cat: " + file + ": No such file or directory");
+            }
+            break;
+        case "ls":
+            output(Array.from(fs.keys()).join("&nbsp;"));
+            break;
         default:
-            output(input + ": command not found");
-
+            output(input.split(" ")[0] + ": command not found");
             break;
     }
     
@@ -70,7 +89,7 @@ function output(thing) {
     // Create a new list item
     const div = document.createElement("div");
     // Add the value to the list item
-    div.innerText = thing;
+    div.innerHTML = thing;
     // Add the list item to the list
     list.appendChild(div);
     // AutoScroll to the newest added List Item
