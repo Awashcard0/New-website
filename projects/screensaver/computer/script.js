5984f9400d1e2b67bafce7bd8145b3752b9e60d2
Awashcard0 <lumpy27@outlook.com>
2025-06-18 00:03:24 -0400

    New screensaver. I made it for my computer so idk how much use it would be for anyone else.

---
+++ projects/screensaver/computer/script.js
@@ -0,0 +1,62 @@
+fetch("https://api.awashcard0.com/api/music/playing")
+    .then(response => response.json())
+    .then(data => {
+        handleMusic(data);
+    });
+
+let music_progress_date
+let music_progress
+let music_state
+
+setInterval(() => {
+    fetch("https://api.awashcard0.com/api/music/playing")
+        .then(response => response.json())
+        .then(data => {
+            handleMusic(data);
+        });
+}, 18000);
+
+setTimeout(() => {
+    document.querySelector(".screensaverMusic").style.display = "block";
+}, 2000);
+    
+
+// https://open.spotify.com/track/1udKn1oNKYQSQ9OmiIWCMu
+
+function handleMusic(data) {
+	const screensaverMusicElement = document.querySelector(".screensaverMusic");
+
+    if (!data.state == "playing" || window.innerWidth < 980) {
+        screensaverMusicElement.style.opacity = "0";
+    } else {
+		randomSSMusicPos(data);
+    }
+}
+
+function randomSSMusicPos(data) {
+    const screensaverMusicElement = document.querySelector(".screensaverMusic");
+	const ssAlbumCover = document.getElementById("ss-album-cover");
+	const ssSongName = document.getElementById("ss-song-name");
+	const ssSongTitle = document.getElementById("ss-song-title");
+
+	screensaverMusicElement.style.opacity = "0";
+
+	setTimeout(() => {
+		ssAlbumCover.src = data.image;
+		ssSongTitle.innerText = `${data.artist} - ${data.album}`;
+		ssSongName.innerText = data.title;
+		screensaverMusicElement.style.right = Math.floor(Math.random() * 67) + "%";
+		screensaverMusicElement.style.top = Math.floor(Math.random() * 60) + "%";
+		screensaverMusicElement.style.opacity = "1";
+	}, 2000);
+}
+
+
+// Add event listener for animation end
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.loader-name').addEventListener('animationend', () => {
+        setTimeout(() => {
+            document.querySelector('.loader').classList.add('hidden');
+        }, 500);
+    });
+});
